eafce253b6550787f8ec04f7f76fe42c3b9eb59f
brunolgb <brunodscolado@gmail.com>
2019-10-09 07:48:31 -0400

    formulario e arquivo que ira registrar

---
+++ src/pages/cadastro-de-calendario/index.php
@@ -0,0 +1,68 @@
+<?php
+    include_once('../../verify_session.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="../../assets/icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="../../style/estilo.css">
+    <title>SEMEC - Cadastro de Calendários</title>
+</head>
+<body>
+   <?php include_once('../header/index.php'); ?>
+
+   <div class="box-control">
+       <div class="box-control-header">
+           <div class="title">
+               <span>PÁGINA INICIAL / CALENDÁRIOS</span>
+               <span>CADASTRO DE CALENDÁRIOS</span>
+           </div>
+           <div class="registration">
+               <a href="../calendario" class='btnPattern'>VOLTAR</a>
+           </div>
+       </div>
+       <div class="box-control-body">
+           <div class="form-pattern">
+               <form action='./registration.php' method="post">
+                   <div class="fieldControl">
+                        <div class="fieldForm tam80">
+                            <label for="calendarName">Nome do Calendário</label>
+                            <input type="text" name='calendarName' id='calendarName' placeholder='Ex: Escolas urbanas'>
+                        </div>
+                        <div class="fieldForm tam20">
+                            <label for="schoolYear">Ano letivo</label>
+                            <input type="text" name='schoolYear' id='schoolYear' placeholder='Ex: 2019' maxlength='4'>
+                        </div>
+                    </div>
+
+                    <div class="locatityCalendar">
+                        <div class="titleLocalityCalendar">Escolha a localidade</div>
+                        <div class='choiseLocality' locality='indigena'>
+                            <img src="../../assets/icone-indigena.svg" alt="">
+                            <span>Indígena</span>
+                        </div>
+                        <div class='choiseLocality' locality='rural'>
+                            <img src="../../assets/icone-rural.svg" alt="">
+                            <span>Rural</span>
+                        </div>
+                        <div class='choiseLocality' locality='urbano'>
+                            <img src="../../assets/icone-urbano.svg" alt="">
+                            <span>Urbano</span>
+                        </div>
+                        <input type="hidden" name="localit" value='' localityValue>
+
+                        <div class="tam100">
+                            <button type="submit" class='submit'>Salvar</button>
+                        </div>
+                    </div>
+               </form>
+           </div>
+       </div>
+   </div>
+
+
+
+   <script src='../../scripts/script.js'></script>
+</body>
+</html>
---
+++ src/pages/cadastro-de-calendario/registration.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+
+include_once('../../class/LoadClass.php');
+
+$storage = array();
+
+foreach ($_POST as $key => $value)
+{
+    if(!empty($value))
+    {
+        $storage[":{$key}"] = $value;
+    }
+}
+
+$storage[":modification_dat"] = Date('Y-m-d H:i:s');
+$storage[":registered_use"] = $_SESSION['id_user'];
+$storage[":finesh"] = 'n';
+
+
+
+$insert = new Insert();
+$comand = "INSERT INTO calendar_information (calendar_name, school_year, locality, modification_date, registered_user, fineshed)
+VALUES ( 
+'{$storage[':calendarName']}',
+'{$storage[':schoolYear']}',
+'{$storage[':localit']}',
+'{$storage[':modification_dat']}',
+'{$storage[':registered_use']}',
+'{$storage[':finesh']}'
+)";
+
+// $comand = "INSERT INTO calendar_information (calendar_name, school_year, locality, modification_date, registered_user, fineshed)
+// VALUES ( 
+// :calendarName,
+// :schoolYear,
+// :localit,
+// :modification_dat,
+// :registered_use,
+// :finesh)";
+
+$return = $insert->inserting(
+    $comand,
+    $storage
+);
+
+if($return)
+{
+    header('Location: ./');
+}
+?>
