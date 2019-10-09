558d893ffa0550f3fda9d1cbfe8114cf16f6160c
brunolgb <brunodscolado@gmail.com>
2019-10-09 07:23:47 -0400

    reorganizando o header e separando seu estilo

---
+++ src/pages/header/index.php
@@ -0,0 +1,33 @@
+<link rel="stylesheet" href="../../style/styleHeader.css">
+  <header class='nav'>
+      <div class="userPerfil">
+        <figure>
+              <img src="../../assets/images-user/icon-user-smaller.png" alt="">
+        </figure>
+      </div>
+      <span>SEMEC</span>
+    <nav>
+        <div class="btnMenu">
+            <div></div>
+            <div class='btnMenuMeio'></div>
+            <div></div>
+        </div>
+        <ul class='menu'>
+            <li>
+                <a href="">Calendários</a>
+            </li>
+            <li>
+                <a href="">Arquivo passivo</a>
+            </li>
+            <li>
+                <a href="">Transferencias</a>
+            </li>
+            <li>
+                <a href="">Perfil</a>
+            </li>
+            <li>
+                <a href="../../../">Sair</a>
+            </li>
+        </ul>
+    </nav>
+</header>
